733776c38b5c83b1ff1c3b871a1e7e183543c233
DerHeldDerSteineFanboy <70917277+DerHeldDerSteineFanboy@users.noreply.github.com>
2020-09-10 22:36:15 +0200

    Add Button funktioniert wieder.
    Außerdem habe ich den text wieder mit den @string... versehen.

--- app/src/main/java/de/leuphana/webmo/foodplan2/structure/Food.java
+++ app/src/main/java/de/leuphana/webmo/foodplan2/structure/Food.java
@@ -1,5 +1,7 @@
 package de.leuphana.webmo.foodplan2.structure;
 
+import java.util.List;
+
 public class Food {
     private int id;
     private String name;
@@ -19,4 +21,18 @@ public class Food {
     }
     public float getPrice(){ return price; }
     public Type getType() {return type; }
+
+    //With this Method we will never get Id´s lower than the maxId
+    //But it´s enough for our context
+    public static int genId() {
+        List<Food> foodArrayList = FoodList.getFoodList().getFoodArrayList();
+        int maxId = 0;
+        for (Food food: foodArrayList) {
+            if (food.id > maxId) {
+                maxId = food.id;
+            }
+            maxId++;
+        }
+        return maxId;
+    }
 }
